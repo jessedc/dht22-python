b38e997ef434e84278a6eb52a6aca20363f657e2
Jesse Collis <jesse@jcmultimedia.com.au>
2019-12-20 15:29:21 +1100

    add startup script

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ cp ./lib/systemd/system/dht22-python.service /lib/systemd/system/
 chmod 644 /lib/systemd/system/dht22-python.service
 
 systemctl daemon-reload
+systemctl disable dht22-python.service
 systemctl enable dht22-python.service
